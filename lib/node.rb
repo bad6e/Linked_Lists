cd773eaad0d0f5aa4d695df4164a16cebbfab439
Bret Doucette <doucette.bret@gmail.com>
2015-07-23 09:03:18 -0600

    Cleaned up code

--- lib/node.rb
+++ lib/node.rb
@@ -12,13 +12,3 @@ class Node
   end
 end
 
-
-# p node_1 = Node.new(1)
-# p node_2 = Node.new(2)
-# p node_3 = Node.new(3)
-# p node_4 = Node.new(4)
-
-# node_1.next = node_2
-# node_2.next = node_3
-# node_3.next = node_4
-
